872dba81a0a01204a38049f6107fd84198f13c5f
Noelia García <noegarciamar505@gmail.com>
2025-09-16 18:12:22 +0200

    [wip] continuación de la página de experiencia

--- src/Experience.vue
+++ src/Experience.vue
@@ -11,30 +11,30 @@
         <!-- Main -->
         <section class="flex justify-center items-start mt-4 mb-12">
 
-            <main class="flex flex-col md:flex-row justify-center items-center md:items-start space-y-6 space-x-4 md:space-y-0 md:space-x-6 px-4 md:px-0">
+            <main
+                class="flex flex-col md:flex-row justify-center items-center md:items-start space-y-6 space-x-4 md:space-y-0 md:space-x-6 px-4 md:px-0">
 
-                <div class="border-[#1A6ECE] border-2 rounded-[35px] p-12 max-w-md">
+                <div class="border-[#1A6ECE] border-2 rounded-[35px] p-12 w-[565px] min-h-[468px]">
                     <h1 class="title flex justify-center">
                         Tecnologías
                     </h1>
-                    <ul class="space-y-2">
-                        <li v-for="tech in technologies" :key="tech.name" class="flex items-center techs">
-                            <span class="text-xl mr-2">
+                    <ul class="grid grid-cols-2 gap-y-2 w-full">
+                        <li v-for="(tech, idx) in technologies" :key="tech.name" class="flex items-center techs" :class="[
+                            idx % 2 === 0
+                                ? 'justify-start flex-row'
+                                : 'flex-row-reverse ml-auto'
+                        ]">
+                            <span class="text-xl mx-2">
                                 <i v-if="tech.emoji === '3'" class="fa-regular fa-face-grin-beam"></i>
                                 <i v-else-if="tech.emoji === '2'" class="fa-regular fa-face-grin"></i>
                                 <i v-else class="fa-regular fa-face-smile"></i>
                             </span>
                             <span>{{ tech.name }}</span>
-                            <em class="ml-auto text-sm techs" v-if="tech.emoji === '3'"> (Nivel
-                                experto)</em>
-                            <em class="ml-auto text-sm techs" v-else-if="tech.emoji === '2'"> (Nivel
-                                intermedio)</em>
-                            <em class="ml-auto text-sm techs" v-else> (Nivel básico)</em>
                         </li>
                     </ul>
                 </div>
 
-                <div class="border-[#1A6ECE] border-2 rounded-[35px] p-12 max-w-md">
+                <div class="border-[#1A6ECE] border-2 rounded-[35px] p-12 w-[565px] min-h-[468px]">
                     <h1 class="title flex justify-center">
                         Experiencia
                     </h1>
@@ -42,8 +42,8 @@
                         <h2 class="techs">
                             Desarrolladora Web
                             <em class="text-[23px] text-gray-500 project-subtitle">
-                            @ Grupo Auro
-                        </em>
+                                @ Grupo Auro
+                            </em>
                         </h2>
                     </div>
                     <p class="text-gray-600 leading-relaxed project-description mt-2 text-justify">
@@ -70,12 +70,18 @@ import Title from './components/Title.vue'
 import Footer from './components/Footer.vue'
 
 const technologies = [
-    { name: 'HTML', emoji: '3' },
-    { name: 'CSS', emoji: '3' },
+    { name: 'HTML & CSS', emoji: '3' },
+    { name: 'TailwindCSS', emoji: '2' },
+    { name: 'Bootstrap', emoji: '2' },
+    { name: 'SASS', emoji: '2' },
     { name: 'JavaScript', emoji: '3' },
-    { name: 'Figma', emoji: '2' },
-    { name: 'Vue', emoji: '1' },
-]
+    { name: 'Vue.js', emoji: '1' },
+    { name: 'PHP & Symfony', emoji: '2' },
+    { name: 'SQL', emoji: '2' },
+    { name: 'Git & Github', emoji: '2' },
+    { name: 'Figma', emoji: '1' },
+    
+].sort((a, b) => Number(b.emoji) - Number(a.emoji));
 
 export default {
     name: 'Experience',
